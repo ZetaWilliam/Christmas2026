7544ea097bf0fc0329a685436b68151f11866628
Zack Wang <152456894+ZetaWilliam@users.noreply.github.com>
2026-09-22 23:53:32 +1200

    Remove temporary environment diagnostic endpoint

--- api/envcheck.js
+++
@@ -1,8 +0,0 @@
-module.exports = async function handler(req, res) {
-  res.status(200).json({
-    databaseUrlPresent: Boolean(process.env.DATABASE_URL),
-    adminPasscodePresent: Boolean(process.env.ADMIN_PASSCODE),
-    vercelEnv: process.env.VERCEL_ENV || null,
-    vercelProject: process.env.VERCEL_PROJECT_PRODUCTION_URL || null
-  });
-};
